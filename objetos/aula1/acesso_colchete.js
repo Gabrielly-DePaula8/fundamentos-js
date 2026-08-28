c48e0752427acf7ff61ddeda007875a52fd512f4
Gabrielly-DePaula8 <gabrielly.pereira.paula@escola.pr.gov.br>
2026-08-28 11:27:26 +0000

    tipos

---
+++ objetos/aula1/acesso_colchete.js
@@ -0,0 +1,11 @@
+const cliente = {
+    nome: "Gaby",
+    idade: 16,
+    cpf: "1122233345",
+    email: "gaby@dominio.com",
+};
+
+console.log('O nome do cliente é ${cliente["nome"]} e essa pessoa tem, $(cliente["idade"]} anos.' );
+
+const chaves = ["nome", "idade". "cpf", "email"];
+
---
+++ objetos/aula2/tipos.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "Gaby",
+    idade: 16,
+    email: "gaby@firma.com",
+    telefone: ["42555544444", "42999885544"],
+}
+
+console.log(cliente);
